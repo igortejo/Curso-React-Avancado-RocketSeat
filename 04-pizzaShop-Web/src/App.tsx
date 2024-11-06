0629856298bced7f1ab481d18208623d8de00fed
igortejo <igortejo5@gmail.com>
2024-11-06 18:56:16 -0300

    Subindo toda aplicação integrada com o back

--- 04-pizzaShop-Web/src/App.tsx
+++ 04-pizzaShop-Web/src/App.tsx
@@ -1,6 +1,24 @@
+import './global.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { Toaster } from  'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
+
 export function App() {
 
   return (
-    <h1>Hello World!</h1>
+    <HelmetProvider>
+      <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
+        <Helmet titleTemplate='%s | pizza.shop'/> {/*colocar um nome na aba*/} 
+        <Toaster richColors  />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </HelmetProvider>
   )
-}
+  
+}  
---
+++ 04-pizzaShop-Web/src/api/mocks/get-profile-mock.ts
@@ -0,0 +1,16 @@
+import { http, HttpResponse } from 'msw'
+import { GetProfileResponse } from '../get-profile'
+
+export const getProfileMock = http.get<never, never, GetProfileResponse>(
+    '/me', () => {
+        return HttpResponse.json({
+            id: 'custom-user-id',
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            phone: '83996532895',
+            role: 'manager',
+            createdAt: new Date(),
+            updatedAt: null,
+
+        })
+    })
